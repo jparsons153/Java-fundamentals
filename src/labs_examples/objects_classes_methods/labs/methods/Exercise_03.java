f4d7688bcf65cf4fdd18719a1319404688caa3a3
jparsons153 <jparsons153@hotmail.com>
2021-08-19 05:41:19 +0100

    recusion exercise 03

--- src/labs_examples/objects_classes_methods/labs/methods/Exercise_03.java
+++ src/labs_examples/objects_classes_methods/labs/methods/Exercise_03.java
@@ -10,11 +10,19 @@ package labs_examples.objects_classes_methods.labs.methods;
 public class Exercise_03 {
 
     public static void main(String[] args) {
-//        int x = factorial(5);
-//        System.out.println(x);
+        int x = factorial(5);
+       System.out.println(x);
 //        should print 120
     }
-
+    static int factorial(int x){
+        int total;
+        if(x==1)
+            return 1;
+        // otherwise, factorial is called and passed x-1,
+        // reducing x each time until it reaches 1.
+        total = factorial(x-1) * x;
+        return total;
+    }
 
 
 
