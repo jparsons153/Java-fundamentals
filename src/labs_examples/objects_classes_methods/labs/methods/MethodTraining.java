8bf54bb9f17c866281ad2ecb21ebb174be1cac45
jparsons153 <jparsons153@hotmail.com>
2021-08-22 12:33:28 +0100

    methods overloading & passing by value

---
+++ src/labs_examples/objects_classes_methods/labs/methods/MethodTraining.java
@@ -0,0 +1,45 @@
+package labs_examples.objects_classes_methods.labs.methods;
+
+public class MethodTraining {
+// 1) method overloading
+
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+
+    public static int addition(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static int addition(double a, double b, double c) {
+        return ((int) (a + b + c));
+    }
+}
+
+// 2) pass by value vs pass by reference
+// pass by value
+class PassingValues {
+    public static void main(String[] args) {
+        double a = 12;
+        double b = 14;
+
+        System.out.println("before a:" + a);
+        System.out.println("before b:" + b);
+
+        someOtherMethod(a, b);
+
+        System.out.println("before a:" + a);
+        System.out.println("before b:" + b);
+    }
+    public static void someOtherMethod(double a, double b){
+        a = a*10;
+        System.out.println(a);
+        b = b * 20;
+        System.out.println(b);
+    }
+}
+
+
+
+
+
