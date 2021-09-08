5e1413059f0aa2e6677596dc0935ede38df19fd8
jparsons153 <jparsons153@hotmail.com>
2021-09-08 07:47:00 +0100

    docs-- added answer to exercise 02

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_02.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_02.java
@@ -3,8 +3,8 @@ package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
 /**
  * Run the class below and study the output. What prints and why?
  *
- * Your answer:
- *
+ * Your answer: subclass has direct access to all public and protected instance variables as though they were their own
+ * i is set in class A and this extends to class B. B can't override instance variables from the parent class.
  */
 class A {
     int i = 10;
