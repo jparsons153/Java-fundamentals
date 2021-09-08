f18aaa35e7a9b2762bf89d4a749152ec934b3469
jparsons153 <jparsons153@hotmail.com>
2021-09-08 09:17:11 +0100

    oop - A_inheritance exercises

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_03.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_03.java
@@ -3,7 +3,7 @@ package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
 /**
  * Why does the output print in the order it does?
  *
- * You answer:
+ * You answer: Parent must always be defeined before the child. Code executes from top to bottom.
  *
  */
 
