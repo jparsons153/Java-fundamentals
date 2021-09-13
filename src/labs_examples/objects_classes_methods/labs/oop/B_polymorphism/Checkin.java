ec7ad6a6973319ed86e4c9b68e9b8a3b54f29544
jparsons153 <jparsons153@hotmail.com>
2021-09-13 05:50:00 +0100

    feat-- exercise 01 part 02 checkin interface & Hotel class added

---
+++ src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/Checkin.java
@@ -0,0 +1,9 @@
+package labs_examples.objects_classes_methods.labs.oop.B_polymorphism;
+
+// Exercise 01 part 02
+
+public interface Checkin {
+    public int reserveRoom();
+    public void bookBreakfast(int roomNumber);
+    public void carryLuggage();
+}
