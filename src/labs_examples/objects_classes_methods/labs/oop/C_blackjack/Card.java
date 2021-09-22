7d0e7179ad5892f766c37de6d7097948cc45725c
jparsons153 <jparsons153@hotmail.com>
2021-09-22 07:24:33 +0100

    feat -- card method

--- src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/Card.java
+++ src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/Card.java
@@ -1,11 +1,17 @@
 package labs_examples.objects_classes_methods.labs.oop.C_blackjack;
 
 public class Card { // holds card data
-    char[] suit = new char[]{'♠', '♦', '♥', '♣'};
-    int cardValue;
+    public char[] suit = new char[]{'♠', '♦', '♥', '♣'};
+    public int cardValue;
+    public char[] faceCard = new char[]{'J', 'K', 'Q', 'A'};
 
     public Card(char[] suit, int cardValue) {
         this.suit = suit;
         this.cardValue = cardValue;
+    }
+
+    public Card(char[] suit, char[] faceCard) {
+        this.suit = suit;
+        this.faceCard = faceCard;
     }
 }
--- src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/Deck.java
+++ src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/Deck.java
@@ -6,6 +6,20 @@ public class Deck { //holds deck data
     Card[] cards;
     ArrayList<Integer> usedCards;
 
+    public void populateDeck(){
+        Card card[] = new Card[52];
+        for (int k=1; k <53; k++){
+            for (int j =0; j<5; j++){ // suit char index
+                for (int i = 2; i < 11; i++) { // populate deck for cards 2-10 for ea suit
+                card[k] = new Card(suit[j],i); //how to loop thru char array??
+            }
+                for (int l =0; l<5; l++) { // loop thru faceCard for each suit
+                // insert method
+                }
+        }
+        }
+    }
+
     public Deck(Card[] cards, ArrayList<Integer> usedCards) {
         this.cards = cards;
         this.usedCards = usedCards;
