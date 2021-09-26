8c51eed9e865993bc73c1209723043d5b3954dce
jparsons153 <jparsons153@hotmail.com>
2021-09-26 10:15:36 +0100

    feat--check if handValue equal = 21

--- src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/Hand.java
+++ src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/Hand.java
@@ -11,20 +11,26 @@ public class Hand {
         this.handValue = handValue;
     }
 
-    public static int handScore(ArrayList<Card> cards, int handValue){
-        for(Card n : cards){ //for each card do the following
-                             // check if cardNumber or faceCard
-            if(cards[n].getfaceCard = 'A'){ // if faceCard = A add 1 to handValue
-                handValue=+1;
-            }
+    public static int handScore(ArrayList<Card> cards, int handValue) {
+        for (Card n : cards) { //for each card do the following
 
-            else if(cards[n].getcardNumber < 11){ // if cardNumber add int to handValue
-                handValue=+ cards[n].getcardNumber;
-            }
-
-            else { // else faceCard is K, Q or J add 10 to handValue
-                handValue=+10;
-            }
+            if (cards[n].getfaceCard = 'A') { // if faceCard = A add 1 to handValue
+                handValue = +1;
+            } else if (cards[n].getcardNumber < 11) { // if cardNumber add int to handValue
+                handValue = +cards[n].getcardNumber;
+            } else { // else faceCard is K, Q or J add 10 to handValue
+                handValue = +10;
             }
         }
+        return handValue;
     }
+
+    public static boolean greaterthan21(int handValue) {
+        if (handValue ==21) {
+            return true;
+        }
+        else return false;
+    }
+}
+
+
