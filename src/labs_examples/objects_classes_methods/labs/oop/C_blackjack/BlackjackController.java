d2a39f51156e2ded2ee21cfe577eb985babeb02f
jparsons153 <jparsons153@hotmail.com>
2021-10-10 13:10:15 +0100

    feat-- methods of handScore & print hand, value of 'A' is 11, when other faceCard drawn. General tidy & refactor

--- src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/BlackjackController.java
+++ src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/BlackjackController.java
@@ -20,7 +20,7 @@ public class BlackjackController {
         char requestCard;
         Boolean AIbool;
 
-        Deck fullDeck = new Deck();
+        Deck fullDeck = new Deck(); // populate a new deck
         fullDeck.populateDeck();
 
         Player computer = new Player("computer", 0, 0); // create computer player
@@ -29,106 +29,144 @@ public class BlackjackController {
         String input = scanner.nextLine();
         Player player = new Player(input, 100, 0); // create player object with name input from console
 
-        System.out.println("Initial pot value " + player.getPotValue());
+        System.out.println("Initial pot value $" + player.getPotValue());
+
+        Scanner takeBet = new Scanner(System.in);
+        System.out.print("Enter bet $"); // take a bet from the player
+        player.setBet(takeBet.nextInt());
+        System.out.println("\n");
+
+        for (int i = 1; i < 3; i++) { // deal player & computer two cards each
+            deal(player, fullDeck);
+            deal(computer, fullDeck);
+        }
 
         do {
-
-            Scanner takeBet = new Scanner(System.in);
-            System.out.print("Enter bet ");
-            player.setBet(takeBet.nextInt());
-
-            deal(player, fullDeck);
-                System.out.println("\n" + "Player has a hand of ");
-                for (Card n : player.hand.get_Cards()) { // print each Card object in playerCards
-                    System.out.println(n);
-                }
-            Hand.handScore(player.hand.get_Cards(), player.hand.handValue); // call handScore method to calculate value of hand
-            System.out.println("\n" + "Players hand value = " + player.hand.handValue); // print handValue & if >21
-            Boolean bool = Hand.greaterthan21(player.hand.handValue);
-            System.out.println("Is players hand greater than 21 " + bool);
-
-            // where bool used? only continue if < 21
+            handScore(player); //calculate handScore
+            handCards(player); //print hand and print if > 21
+            Boolean bool = greaterthan21(player.hand);
+            System.out.println("Is players hand greater than or equal to 21 " + bool);
+            if (bool) {
+                break;
+            }
 
             Scanner newCard = new Scanner(System.in);
             System.out.print("Request another card Y/N: ");
             requestCard = scanner.next().charAt(0);
-
-            deal(computer, fullDeck);
-            Hand.handScore(computer.hand.get_Cards(), 0);
-
-            AIbool = computer.computerAI(computer.hand.handValue);
-                if (AIbool) {
-                    System.out.println("Computer has taken another card");
-                }
-
-        } while ((requestCard == 'Y' & AIbool) | (player.hand.handValue < 21 | computer.hand.handValue < 21)); //continue play if requested another card & handvalue < 21
-
-        // create method for printing hand + handValue??
-
-        System.out.println("User " + player.getName() + "has a hand of: ");
-            for (Card n : player.hand.get_Cards()) { // print each Card object in playerCards
-                System.out.println(n);
+            if (requestCard == 'Y') { // if request another card, deal one more
+                deal(player, fullDeck);
             }
-        System.out.println("with a hand score of " + player.hand.handValue);
+            else {
+                break;
+            }
 
-        System.out.println("Computer " + computer.getName() + "has a hand of: ");
-            for (Card n : computer.hand.get_Cards()) { // print each Card object in playerCards
-                System.out.println(n);
-            }
-        System.out.println("with a hand score of " + computer.hand.handValue);
+            handScore(computer);
 
-            if(player.hand.handValue > computer.hand.handValue && player.hand.handValue < 21){
-                player.potValue = 2* player.getBet();
-                System.out.println("\n Player has won!!");
-                System.out.println("Your potValue = " + player.getPotValue());
+            AIbool = computer.computerAI(computer.hand.handValue); //if computer hand < 16, request another card
+            if (AIbool) {
+                System.out.println("Computer has taken another card");
+                deal(computer, fullDeck);
+            } else if (computer.hand.handValue >= 21) {
+                break;
             }
-            else if(player.hand.handValue > computer.hand.handValue && player.hand.handValue == 21) {
-                player.potValue = 2.5* player.getBet();
-                System.out.println("\n Player has hit BlackJack, congratulations!!");
-                System.out.println("Your potValue = " + player.getPotValue());
-            }
-            else if(computer.hand.handValue > player.hand.handValue && computer.hand.handValue <= 21) {
-                player.potValue = 0;
-                System.out.println("\n Computer has won, try again!! \n");
-            }
-            else if ((player.hand.handValue == computer.hand.handValue && player.hand.handValue < 21)) {
-                player.potValue = player.getBet();
-                System.out.println("\n Draw \n");
-            }
-            else{
-                player.potValue = 0;
-                System.out.println("\n Try again \n");
-            }
+
+        } while ((player.hand.handValue < 22 | computer.hand.handValue < 22)); //continue play if requested another card & handvalue < 21
+        //} while (player.getBet() < player.potValue);
+
+        handScore(player); //calculate handScore, print hand and print if > 21
+        handScore(computer);
+
+        if (player.hand.handValue > computer.hand.handValue && player.hand.handValue < 21) {
+            player.potValue = 2 * player.getBet();
+            System.out.println("\n" +"Player has won!!");
+            System.out.println("Your potValue = $" + player.getPotValue());
         }
+        else if (player.hand.handValue == 21) {
+            player.potValue = 2.5 * player.getBet();
+            System.out.println("\n" +"Player has hit BlackJack, congratulations!!");
+            System.out.println("Your potValue = $" + player.getPotValue());
+        }
+        else if (computer.hand.handValue > player.hand.handValue && computer.hand.handValue <= 21) {
+            player.potValue = 0;
+            System.out.println("\n" +"Computer has won, try again!!" +"\n");
+        }
+        else if ((player.hand.handValue == computer.hand.handValue && player.hand.handValue < 21)) {
+            player.potValue = player.getBet();
+            System.out.println("\n" +"Draw"+ "\n");
+        }
+        else {
+            player.potValue = 0;
+            System.out.println("\n" +"Try again"+ "\n");
+        }
+    }
 
     public static void deal(Player player, Deck fullDeck) {
         int min = 1;
         int max = 52;
+        int random_int;
 
-            for (int i = 1; i < 3; i++) { // deal player two cards
+        do {
+            random_int = (int) Math.floor(Math.random() * (max - min + 1) + min);
+            System.out.println("Card number " + random_int + " drawn");
 
-                int random_int = (int) Math.floor(Math.random() * (max - min + 1) + min);
-                System.out.println("\n" + "Card number " + random_int + " drawn");
+            Card[] cards = fullDeck.getCards();
+            Card myCard = cards[random_int];
 
-                Card[] cards = fullDeck.getCards();
-                Card myCard = cards[random_int];
+            Hand myhand = player.hand;
+            ArrayList<Card> myCards = myhand.cards;
 
-                System.out.println("which is " + myCard); // find card from Card array in deck class
+            myCards.add(myCard); // add card object from array at random number to playerCards Arraylist
 
-                if (fullDeck.getUsedCards().contains(random_int)) { //check if card has already been drawn if so request another card
-                    continue;
-                }
+            fullDeck.getUsedCards().add(random_int);   // add card number to usedCard arraylist
 
+        } while (!fullDeck.getUsedCards().contains(random_int));
+    }
 
-                Hand myhand = player.hand;
-                ArrayList<Card> myCards = myhand.cards;
+    public static int handScore(Player player) {
 
-                myCards.add(myCard); // add card object from array at random number to playerCards Arraylist
+        for (Card card : player.hand.cards) { //for each card do the following
 
-                fullDeck.getUsedCards().add(random_int);   // add card number to usedCard arraylist
+            if (card.getCardValue() > 1 && card.getCardValue() < 11) { // if cardNumber add int to handValue
+                player.hand.handValue += card.getCardValue();
+            } else if (card.getFaceCard() == 'A') { // if faceCard = A add 1 to handValue
+                player.hand.handValue += 1;
+            } else { // else faceCard is K, Q or J add 10 to handValue
+                player.hand.handValue += 10;
             }
-        //} while (player.getBet() < player.potValue);
+        }
+        valueOfAce(player);
 
+        return player.hand.handValue;
+    }
+
+    public static int valueOfAce(Player player) {
+        boolean Ace = false;
+        boolean faceCard = false;
+
+        for (Card card : player.hand.cards) { //for each card do the following
+
+            if (card.getFaceCard() == 'A') {
+                Ace = true;
+            }
+            else if (card.getFaceCard() == 'K' | card.getFaceCard() == 'Q' | card.getFaceCard() == 'J') {
+                faceCard = true;
+            }
+        }
+        if (Ace && faceCard) {
+            player.hand.handValue += 10;
+        }
+        return player.hand.handValue;
+    }
+
+    public static void handCards(Player player){
+        System.out.println("\n" +player.getName()+ " has a hand of ");
+        for (Card n : player.hand.get_Cards()) { // print each Card object in playerCards
+            System.out.println(n);
+        }
+        System.out.println("\n" + player.getName() +" hand value = " + player.hand.handValue); // print handValue & if >21
+    }
+
+    public static boolean greaterthan21(Hand hand) {
+        return hand.handValue >= 21; // return true if >=21 otherwise return false
     }
 }
-
--- src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/Hand.java
+++ src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/Hand.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Hand {
     ArrayList<Card> cards;
-    int handValue = 0;
+    int handValue =0;
 
     public Hand(ArrayList<Card> cards, int handValue) {
         this.cards = cards;
@@ -19,26 +19,6 @@ public class Hand {
         return cards;
     }
 
-
-    public static int handScore(ArrayList<Card> cards, int handValue) {
-        for (Card card : cards) { //for each card do the following
-
-            if (card.getFaceCard() == 'A') { // if faceCard = A add 1 to handValue
-                handValue += 1;
-            }
-            else if (card.getCardValue() < 11) { // if cardNumber add int to handValue
-                handValue += card.getCardValue();
-            }
-            else { // else faceCard is K, Q or J add 10 to handValue
-                handValue += 10;
-            }
-        }
-        return handValue;
-    }
-
-    public static boolean greaterthan21(int handValue) {
-        return handValue >= 21; // return true if >21 otherwise return false
-    }
 }
 
 
