d47fc3f3a6b355fba86b7e67e4b9147cf20f3bcf
jparsons153 <jparsons153@hotmail.com>
2021-11-24 06:39:33 +0000

    Exercise 02 & 03

--- src/labs_examples/exception_handling/labs/Exercise_02.java
+++ src/labs_examples/exception_handling/labs/Exercise_02.java
@@ -7,4 +7,22 @@ package labs_examples.exception_handling.labs;
  *
  */
 
+class Exercise02 {
+
+    public static void main(String[] args) {
+
+        int[] nums = {10, 2, 6, 32};
+
+        try{
+            int c = nums[0] / nums [5];
+        } catch(ArrayIndexOutOfBoundsException error) {
+            System.out.println("ArrayIndex Out of Bounds Exception error");
+        } catch (ArithmeticException error2){
+            System.out.println("Arithmetic Exception error");
+        } catch (Exception exc) {
+            System.out.println("Exception error");
+        }
+    }
+
+}
 
--- src/labs_examples/exception_handling/labs/Exercise_03.java
+++ src/labs_examples/exception_handling/labs/Exercise_03.java
@@ -6,3 +6,23 @@ package labs_examples.exception_handling.labs;
  *      Demonstrate a try/catch/finally block.
  *
  */
+class Exercise03 {
+
+    public static void main(String[] args) {
+
+        int[] nums = {10, 2, 6, 32};
+
+        try {
+            int c = nums[0] / nums[5];
+        } catch (ArrayIndexOutOfBoundsException error) {
+            System.out.println("ArrayIndex Out of Bounds Exception error");
+        } catch (ArithmeticException error2) {
+            System.out.println("Arithmetic Exception error");
+        } catch (Exception exc) {
+            System.out.println("Exception error");
+
+        } finally {
+            System.out.println("Reached end of code block");
+        }
+    }
+}
