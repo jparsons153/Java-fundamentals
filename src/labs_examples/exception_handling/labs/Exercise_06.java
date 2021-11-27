dc09d2bcc9f6884bef2215434322e24d37f0e839
jparsons153 <jparsons153@hotmail.com>
2021-11-27 11:21:56 +0000

    Exercise 06

--- src/labs_examples/exception_handling/labs/Exercise_06.java
+++ src/labs_examples/exception_handling/labs/Exercise_06.java
@@ -7,3 +7,23 @@ package labs_examples.exception_handling.labs;
  *
  */
 
+class Excerise_06 {
+    public static void main(String[] args) {
+        try{
+            methodOne();
+        } catch (ArrayIndexOutOfBoundsException exc){
+            System.out.println("Exception from methodOne caught");
+        }
+    }
+
+    public static void methodOne() throws ArrayIndexOutOfBoundsException {
+        int arr[] = {12, 6, 45};
+        try{
+            int c = arr[3];
+        } catch (ArrayIndexOutOfBoundsException exc){
+            System.out.println("Throwing exception back to main method");
+            throw exc;
+        }
+    }
+}
+
