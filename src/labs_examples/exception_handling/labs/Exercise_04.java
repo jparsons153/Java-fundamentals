90437266d8494aec72dd1bf69fe5049bdd32ca87
jparsons153 <jparsons153@hotmail.com>
2021-11-27 09:45:24 +0000

    exercise 04

--- src/labs_examples/exception_handling/labs/Exercise_04.java
+++ src/labs_examples/exception_handling/labs/Exercise_04.java
@@ -6,3 +6,24 @@ package labs_examples.exception_handling.labs;
  *      Demonstrate a nested try/catch.
  *
  */
+
+class NestedTry {
+
+    public static void main(String[] args) {
+
+        int array[] = {2, 5, 17, 8};
+
+        try {
+            try {
+                System.out.println(array[10]);
+
+            } catch (ArithmeticException exc) {
+                System.out.println("Arithmetic Exception");
+                System.out.println("Try block 2");
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array Index out of bounds exception");
+            System.out.println("Try block 1");
+        }
+    }
+}
