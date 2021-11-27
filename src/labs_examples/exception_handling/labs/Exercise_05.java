dd0dc5f92f48ee1129757f156b9b9fe576c36498
jparsons153 <jparsons153@hotmail.com>
2021-11-27 11:11:07 +0000

    Exercise 05

--- src/labs_examples/exception_handling/labs/Exercise_05.java
+++ src/labs_examples/exception_handling/labs/Exercise_05.java
@@ -13,6 +13,16 @@ class Example {
 
     public static void main(String[] args) {
 
+        int array[] = {2, 0};
+
+        try {
+            int c = divide(array[0], array[1]);
+        } catch (ArithmeticException exc) {
+            System.out.println("Arithmetic exception from divide method");
+        }
     }
 
+public static int divide(int a, int b) throws ArithmeticException {
+    return a/b;
+    }
 }
