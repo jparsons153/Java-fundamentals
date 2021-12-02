3771868fe72d6c2e027b4eb18cf398e745e976df
jparsons153 <jparsons153@hotmail.com>
2021-12-02 05:49:29 +0000

    inputOutput exercise_01

--- src/labs_examples/input_output/labs/Exercise_01.java
+++ src/labs_examples/input_output/labs/Exercise_01.java
@@ -1,5 +1,7 @@
 package labs_examples.input_output.labs;
 
+import java.io.*;
+
 /**
  * Input/Output Exercise 1: File input/output
  *
@@ -12,7 +14,45 @@ package labs_examples.input_output.labs;
 class Example {
     public static void main(String[] args) {
 
+        FileInputStream inputStream = null;
+        FileOutputStream outputStream = null;
+        BufferedInputStream bufferedInputStream = null;
+        BufferedOutputStream bufferedOutputStream = null;
 
+        String inFilePath = "C:\\Users\\User\\Documents\\labs\\online-java-fundamentals\\src\\labs_examples\\input_output\\files\\byte_data";
+        String outFilePath = "C:\\Users\\User\\Documents\\labs\\online-java-fundamentals\\src\\labs_examples\\input_output\\files\\some_output_file.txt";
 
+        try {
+            inputStream = new FileInputStream(inFilePath);
+            bufferedInputStream = new BufferedInputStream(inputStream);
+
+            outputStream = new FileOutputStream(outFilePath);
+            bufferedOutputStream = new BufferedOutputStream(outputStream);
+
+            byte[] buffer = new byte[5];
+            int bytesRead = 0;
+
+            while ((bytesRead = bufferedInputStream.read(buffer)) != -1) {
+                // write contents to a new file
+                outputStream.write(bytesRead);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            // replace with try with resources
+
+            try {
+                inputStream.close();
+                bufferedInputStream.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            try{
+                outputStream.close();
+                bufferedOutputStream.close();
+            } catch (IOException ex){
+                ex.printStackTrace();
+            }
+        }
     }
 }
