97374c16fe1184d1c6e84618250cf73be556a99d
jparsons153 <jparsons153@hotmail.com>
2021-12-07 17:57:07 +0000

    feat-- exercise 04, csv to obj

--- src/labs_examples/input_output/labs/Exercise_04.java
+++ src/labs_examples/input_output/labs/Exercise_04.java
@@ -1,5 +1,13 @@
 package labs_examples.input_output.labs;
 
+import labs_examples.input_output.examples.csv_parser.Student;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  *
  *      Following the video examples and source code found in the src/labs_examples/input_output/examples/csv_parser
@@ -12,3 +20,66 @@ package labs_examples.input_output.labs;
  *      Then, write the arraylist of objects back out to a new .csv file. Ensure that the resulting csv file is valid.
  *
  */
+
+
+// read in csv file - DONE
+// create bufferedReader & parse by ',' - DONE
+// map each line to POJO - DONE
+// add object to arrayList - DONE
+// print object toString - DONE
+// write objects in arrayList to new csv file
+// catch exceptions
+
+class CSV_Parse_Reports {
+
+    public static void main(String[] args) {
+
+        csvToObj();
+
+    }
+    public static void csvToObj(){
+
+    // ArrayList of Reports
+    ArrayList<Report> reports = new ArrayList();
+
+    String filePath = "C:\\Users\\User\\Documents\\labs\\online-java-fundamentals\\src\\labs_examples\\input_output\\files\\Reports.csv";
+
+        try (BufferedReader br =
+                     new BufferedReader(new FileReader(filePath))) {
+
+            String line;
+
+            // parse csv file and add object to Reports arraylist
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                reports.add(mapValuesToReportObj(values));
+            }
+        } catch (
+            FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (
+            IOException e) {
+            e.printStackTrace();
+            }
+
+            // Print toString for each report in Arraylist
+            for(Report report : reports){
+                System.out.println(report.toString());
+            }
+    }
+
+    private static Report mapValuesToReportObj(String[] values){
+        Report report = new Report();
+
+        report.setTool(values[0]);
+        report.setEquipmentNumber(values[1]);
+        report.setAuthor(values[2]);
+        report.setDate(values[3]);
+
+        return report;
+    }
+}
+
+
+
+
---
+++ src/labs_examples/input_output/labs/Report.java
@@ -0,0 +1,44 @@
+package labs_examples.input_output.labs;
+
+public class Report {
+    private String tool;
+    private String equipmentNumber;
+    private String author;
+    private String date;
+
+    public Report() {
+    }
+
+    public Report(String tool, String equipmentNumber, String author, String date) {
+        this.tool = tool;
+        this.equipmentNumber = equipmentNumber;
+        this.author = author;
+        this.date = date;
+    }
+
+    public void setTool(String tool) {
+        this.tool = tool;
+    }
+
+    public void setEquipmentNumber(String equipmentNumber) {
+        this.equipmentNumber = equipmentNumber;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Report{" +
+                "tool='" + tool + '\'' +
+                ", equipmentNumber='" + equipmentNumber + '\'' +
+                ", author='" + author + '\'' +
+                ", date='" + date + '\'' +
+                '}';
+    }
+}
