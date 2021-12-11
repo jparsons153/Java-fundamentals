0bcf96ba24da274664a8cd833243d43a858ab174
jparsons153 <jparsons153@hotmail.com>
2021-12-11 18:07:12 +0000

    Exercise 03 -02 Character lineNumberReader

--- src/labs_examples/input_output/labs/Exercise_03.java
+++ src/labs_examples/input_output/labs/Exercise_03.java
@@ -12,53 +12,101 @@ import java.io.*;
  *
  */
 
+// Exercise 03 - 1) Byte Streams
 class Byte_Streams {
 
     public static void main(String[] args) {
+        // Byte Stream files
+        String file1 = "C:\\Users\\User\\Documents\\labs\\online-java-fundamentals\\src\\labs_examples\\input_output\\files\\byte_data";
+        String file2 = "C:\\Users\\User\\Documents\\labs\\online-java-fundamentals\\src\\labs_examples\\input_output\\files\\random.dat";
 
-    try{
-        addInputStreams();
 
-        pushBackStream();
+            try{
+                addInputStreams(file1,file2);
 
-    } catch (FileNotFoundException ex) {
-        ex.printStackTrace();
+                pushBackStream(file1);
 
-    } catch (IOException exc) {
-        exc.printStackTrace();
+            } catch (FileNotFoundException ex) {
+                ex.printStackTrace();
+
+            } catch (IOException exc) {
+                exc.printStackTrace();
+            }
+
     }
 
-    }
-
-    private static void addInputStreams() throws IOException {
+    private static void addInputStreams(String file1, String file2) throws IOException {
 
         InputStream input1 = null;
         InputStream input2 = null;
 
-        input1 = new FileInputStream("C:\\Users\\User\\Documents\\labs\\online-java-fundamentals\\src\\labs_examples\\input_output\\files\\byte_data");
-        input2 = new FileInputStream("C:\\Users\\User\\Documents\\labs\\online-java-fundamentals\\src\\labs_examples\\input_output\\files\\random.dat");
+        input1 = new FileInputStream(file1);
+        input2 = new FileInputStream(file2);
         SequenceInputStream sequenceInputStream = new SequenceInputStream(input1, input2);
 
         int read = sequenceInputStream.read();
-        while (read != -1) {
-            System.out.println(read);
-            read = sequenceInputStream.read();
-        }
+            while (read != -1) {
+                System.out.println(read);
+                read = sequenceInputStream.read();
+            }
     }
 
-    private static void pushBackStream() throws IOException {
+    private static void pushBackStream(String file1) throws IOException {
 
         // set limit for bytes to be unread
         int limit = 8;
 
-        PushbackInputStream input = new PushbackInputStream(new FileInputStream("C:\\Users\\User\\Documents\\labs\\online-java-fundamentals\\src\\labs_examples\\input_output\\files\\byte_data"), limit);
+        PushbackInputStream input = new PushbackInputStream(new FileInputStream(file1), limit);
 
         int data = input.read();
 
-            byte byteRead = (byte) data;
-            System.out.println("Pushback input stream");
-            System.out.println(byteRead);
-            input.unread(byteRead);
-            data = input.read();
+        // FOR loop - read, unread , read
+        // print to console
+
+        byte byteRead = (byte) data;
+        System.out.println("Pushback input stream");
+        System.out.println(byteRead);
+        input.unread(byteRead);
+        data = input.read();
     }
+}
+
+// Exercise 03 - 2) Character Streams
+
+class CharacterStreams {
+
+    public static void main(String[] args) {
+
+        // Character Stream files
+        String charFile = "C:\\Users\\User\\Documents\\labs\\online-java-fundamentals\\src\\labs_examples\\input_output\\files\\char_data.txt";
+
+        try{
+            lineReader(charFile);
+
+        } catch (FileNotFoundException ex) {
+        ex.printStackTrace();
+
+        } catch (IOException exc) {
+            exc.printStackTrace();
+        }
+
+    }
+
+    public static void lineReader(String charFile) throws IOException {
+
+        LineNumberReader lineNumberReader = new LineNumberReader(new FileReader(charFile));
+
+        int readData = lineNumberReader.read();
+        while(readData != -1){
+            char charRead = (char) readData;
+            readData = lineNumberReader.read();
+            int lineNumber = lineNumberReader.getLineNumber();
+            System.out.println("Reading line" + lineNumber);
+        }
+        System.out.println("File read successfully");
+        lineNumberReader.close();
+    }
+
+// second Character Stream 
+
 }
