620ee5624a4329d6dcb7c66f8e7509bed66900bd
jparsons153 <jparsons153@hotmail.com>
2021-12-14 05:48:33 +0000

    Exercise_02 extending thread

--- src/labs_examples/multi_threading/labs/Exercise_02.java
+++ src/labs_examples/multi_threading/labs/Exercise_02.java
@@ -5,3 +5,33 @@ package labs_examples.multi_threading.labs;
  *
  *      Create an application that creates a Thread using the Thread class
  */
+
+class ThreadController{
+
+    public static void main(String[] args) {
+        ExtendingThread thread1 = new ExtendingThread("Thread_1");
+    }
+}
+
+
+class ExtendingThread extends Thread {
+
+    ExtendingThread(String name){
+        super(name);
+        start();
+    }
+
+    @Override
+    public void run(){
+        System.out.println("Thread" + getName() + " starting...");
+        try{
+            for(int j =0; j <20; j+=2){
+                Thread.sleep(500);
+                System.out.println("Thread " +getName() + " count = " + j);
+            }
+        }catch (InterruptedException ex){
+            ex.printStackTrace();
+        }
+        System.out.println("Thread" + getName() + " ending...");
+    }
+}
