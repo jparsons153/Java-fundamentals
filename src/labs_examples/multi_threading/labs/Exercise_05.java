96eb5c6916619e8a3aa4e1a51f827467de1a9653
jparsons153 <jparsons153@hotmail.com>
2021-12-22 17:07:28 +0000

    Exercise 05 & 06

--- src/labs_examples/multi_threading/labs/Exercise_05.java
+++ src/labs_examples/multi_threading/labs/Exercise_05.java
@@ -6,3 +6,67 @@ package labs_examples.multi_threading.labs;
  *      Demonstrate the use of a wait() and notify()
  */
 
+class WaitNotify {
+    public static void main(String[] args) {
+        DayAndNight day = new DayAndNight();
+        Week thread1 = new Week("WakeUp", day);
+        Week thread2 = new Week("Sleep", day);
+    }
+}
+
+class DayAndNight {
+
+    String currentState = "";
+
+    synchronized void WakeUp(){
+        currentState = "WakeUp";
+        System.out.println(currentState);
+        notify();
+        try{
+            while(currentState.equals("WakeUp")){
+                wait();
+            }
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+    synchronized void Sleep(){
+        currentState = "Sleep";
+        System.out.println(currentState);
+        notify();
+        try{
+            while(currentState.equals("Sleep")){
+                wait();
+            }
+        }catch (InterruptedException ex){
+            ex.printStackTrace();
+        }
+    }
+}
+
+class Week extends Thread {
+    String name;
+    DayAndNight dayref;
+
+    Week(String name, DayAndNight dayref){
+        this.name = name;
+        this.dayref = dayref;
+        setName(name);
+        start();
+    }
+
+    @Override
+    public void run(){
+        if(name.equals("WakeUp")){
+            for (int i =0; i<8; i++){
+                dayref.WakeUp();
+            }
+        }
+        if (name.equals("Sleep")){
+            for (int i =0; i<8; i++){
+                dayref.Sleep();
+            }
+        }
+    }
+}
--- src/labs_examples/multi_threading/labs/Exercise_06.java
+++ src/labs_examples/multi_threading/labs/Exercise_06.java
@@ -2,7 +2,77 @@ package labs_examples.multi_threading.labs;
 
 /**
  * Multithreading Exercise 6:
- *
  *      Write a program that will print 1-100 sequentially from at least two distinct threads. For instance, thread1 will
  *      print "1", then thread2 will print "2", then thread1 will print "3", then thread2 will print "4" and so on.
  */
+
+class WaitOrNotify{
+    public static void main(String[] args) {
+
+        Count obj = new Count();
+
+       MainThread thread1 = new MainThread("counter1", obj);
+       MainThread thread2 = new MainThread ("counter2", obj);
+    }
+}
+
+class Count {
+    static int currentCount =0;
+    String counter;
+
+   synchronized void Counter1Count() {
+       counter = "counter1";
+       System.out.println(counter);
+       currentCount +=1;
+       System.out.println(currentCount);
+       notify();
+           try {
+               while (counter.equals("counter1")) {
+                   wait();
+               }
+           } catch (InterruptedException ex) {
+               ex.printStackTrace();
+           }
+   }
+
+   synchronized void Counter2Count(){
+       counter = "counter2";
+       System.out.println(counter);
+       currentCount +=1;
+       System.out.println(currentCount);
+       notify();
+           try {
+               while (counter.equals("counter2")) {
+                   wait();
+               }
+           } catch (InterruptedException e) {
+               e.printStackTrace();
+           }
+   }
+}
+
+   class MainThread extends Thread {
+       String name;
+       Count countobj;
+
+       MainThread(String name, Count countobj) {
+           this.name = name;
+           this.countobj = countobj;
+           setName(name);
+           start();
+       }
+
+       @Override
+       public void run() {
+
+           for (int i = 1; i < 51; i++) {
+
+               if (name.equals("counter1")) {
+                   countobj.Counter1Count();
+               }
+               if (name.equals("counter2")) {
+                   countobj.Counter2Count();
+               }
+           }
+       }
+   }
