0e3a85008a52860e84fb02295a21580f6175fb9d
jparsons153 <jparsons153@hotmail.com>
2021-12-24 11:11:26 +0000

    Exercise -02 - add items of an Arraylist

--- src/labs_examples/generics/labs/Exercise_02.java
+++ src/labs_examples/generics/labs/Exercise_02.java
@@ -2,12 +2,32 @@ package labs_examples.generics.labs;
 
 /**
  * Generics Exercise 2:
- *
- *      Create a class with a generic method that takes in an ArrayList of any Numeric type and returns the sum of all
- *      Numbers in the ArrayList. Demonstrate how to call this method from the main() method.
+ * <p>
+ * Create a class with a generic method that takes in an ArrayList of any Numeric type and returns the sum of all
+ * Numbers in the ArrayList. Demonstrate how to call this method from the main() method.
  */
+
 import java.util.ArrayList;
 
-class Demo{
+class Demo {
 
+    public static void main(String[] args) {
+
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        array.add(5);
+        array.add(10);
+        array.add(20);
+
+        int result = (int) sumArray(array);
+        System.out.println(result);
+    }
+
+    public static <T extends Number> int sumArray(ArrayList<T> arra) {
+        int total = 0;
+
+        for (T ele : arra) {
+            total = ele.intValue() + total;
+        }
+        return total;
+    }
 }
