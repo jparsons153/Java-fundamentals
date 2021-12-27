f539d0e0c9c559a1a81889b4ae38c5029901a543
jparsons153 <jparsons153@hotmail.com>
2021-12-27 16:09:33 +0000

    Exercise 01 - Enums

--- src/labs_examples/enumerations/labs/Exercise_01.java
+++ src/labs_examples/enumerations/labs/Exercise_01.java
@@ -1,5 +1,7 @@
 package labs_examples.enumerations.labs;
 
+import java.util.Scanner;
+
 /**
  * Enumerations Exercise 1:
  *
@@ -8,3 +10,49 @@ package labs_examples.enumerations.labs;
  *      of this enum from a seperate class.
  */
 
+enum Month{
+    January (1, 31),
+    February (2, 28),
+    March (3, 31),
+    April (4, 30),
+    May (5, 31),
+    June (6, 30),
+    July (7, 31),
+    August (8, 31),
+    September (9, 30),
+    October(10, 31),
+    November (11, 30),
+    December (12, 31);
+
+    private final int monthNumber;
+    private final int calendarDays;
+
+    Month(int monthNumber, int calendarDays) {
+        this.monthNumber = monthNumber;
+        this.calendarDays = calendarDays;
+    }
+
+    public int getMonthNumber() {
+        return monthNumber;
+    }
+
+    public int getCalendarDays() {
+        return calendarDays;
+    }
+}
+
+class MonthController {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter month to return number of calendar days: ");
+        String s = scanner.next();
+
+        for(Month m: Month.values()){
+            if(m.equals(s)) {
+                System.out.println(m.getCalendarDays());
+            }
+        }
+    }
+}
