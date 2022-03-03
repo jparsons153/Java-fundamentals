46c5297f75a9c5d95aa9ee9c9ed24534ca13bfd1
jparsons153 <jparsons153@hotmail.com>
2022-03-03 07:34:29 +0000

    Hashmap exercise 02 - created linked list + methods

--- src/labs_examples/datastructures/hashmap/labs/Exercise_03.java
+++ src/labs_examples/datastructures/hashmap/labs/Exercise_03.java
@@ -1,5 +1,8 @@
 package labs_examples.datastructures.hashmap.labs;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 /**
  *      HashMaps Exercise_02
  *
@@ -19,3 +22,48 @@ package labs_examples.datastructures.hashmap.labs;
  *      3) search for 100 elements
  *      4) delete 100 elements
  */
+
+class HashmapsExercise02 {
+
+    public static void main(String[] args) {
+
+        linkedList();
+    }
+
+    // linkedlist
+    public static LinkedList<Integer> linkedList(){
+
+        // create a LinkedList of type String
+        LinkedList<Integer> linkedList = new LinkedList<Integer>();
+
+        // add 100 elements to the LinkedList
+        for (int i=0; i<101; i++) {
+            linkedList.add(i);
+        }
+
+        // update each element
+        for (int i=0; i<101; i++) {
+            linkedList.set(i,i+10);
+        }
+
+        // search for each element
+        for(int j=100; j>0; j--){
+            boolean searchList = linkedList.contains(j+10);
+            System.out.println("\n\nList contains \"LinkedList\": " + searchList);
+        }
+
+        // delete each element
+        // TO DO fix index out of bounds
+        for (int k=0; k<101; k++) {
+            linkedList.remove(k);
+        }
+
+        // return LinkedList
+/*
+        Iterator i = linkedList.iterator();
+        while(i.hasNext()){
+            System.out.print(i.next()+ " ");
+        }
+*/        return linkedList;
+    }
+}
