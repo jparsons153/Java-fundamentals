dede17eced826ac23ce9fd815073180d412492f7
jparsons153 <jparsons153@hotmail.com>
2022-03-07 06:48:21 +0000

    feat -- timers added

--- src/labs_examples/datastructures/hashmap/labs/Exercise_03.java
+++ src/labs_examples/datastructures/hashmap/labs/Exercise_03.java
@@ -1,5 +1,7 @@
 package labs_examples.datastructures.hashmap.labs;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.*;
 
 /**
@@ -26,6 +28,8 @@ class HashmapsExercise02 {
 
     public static void main(String[] args) {
 
+        Instant start = Instant.now();
+
         linkedList();
 
         intStack();
@@ -33,6 +37,12 @@ class HashmapsExercise02 {
         intQueue();
 
         intHashmap();
+
+        Instant finish = Instant.now();
+        long runAll = Duration.between(start,finish).toNanos();
+        System.out.println("Run all 4 datastructures; LinkedList, Stack, Queue & HashMap " + runAll + " nano seconds");
+        // Timer changed to Nano, milli doesn't give sufficient resolution
+        // Nano seconds doesn't give much better resolution!
     }
 
     // linkedlist
@@ -40,10 +50,14 @@ class HashmapsExercise02 {
 
         LinkedList<Integer> linkedList = new LinkedList<Integer>();
 
+        // start timer for linkedList
+        Instant start = Instant.now();
+
         // add 100 elements to the LinkedList
         for (int i = 0; i < 101; i++) {
             linkedList.add(i);
         }
+
 
         // update each element
         for (int i = 0; i < 101; i++) {
@@ -53,11 +67,16 @@ class HashmapsExercise02 {
         // search for each element
         for (int j = 100; j > 0; j--) {
             boolean searchList = linkedList.contains(j + 10);
-            System.out.println("List contains " + j + " " + searchList);
+           // System.out.println("List contains " + j + " " + searchList);
         }
 
         // delete each element
         linkedList.clear();
+
+        // stop timer
+        Instant finish = Instant.now();
+        long listTimer = Duration.between(start,finish).toNanos();
+        System.out.println("Time to execute Linkedlist tasks " + listTimer + " nano seconds");
 
         return linkedList;
     }
@@ -65,6 +84,9 @@ class HashmapsExercise02 {
     // stack
     public static Stack<Integer> intStack() {
         Stack<Integer> intStack = new Stack();
+
+        // start timer for linkedList
+        Instant start = Instant.now();
 
         // add 100 elements
         for (int i = 0; i < 101; i++) {
@@ -79,7 +101,7 @@ class HashmapsExercise02 {
         // search for each element
         for (int j = 100; j > 0; j--) {
             int searchStack = intStack.search(j + 20);
-            System.out.println("Stack contains " + j + " " + searchStack);
+           // System.out.println("Stack contains " + j + " " + searchStack);
         }
 
         // delete each element
@@ -87,12 +109,20 @@ class HashmapsExercise02 {
             intStack.pop();
         }
 
+        // stop timer
+        Instant finish = Instant.now();
+        long stackTimer = Duration.between(start,finish).toNanos();
+        System.out.println("Time to execute Stack tasks " + stackTimer + " nano seconds");
+
         return intStack;
     }
 
     // Queue
     public static Queue<Integer> intQueue() {
         PriorityQueue<Integer> intQueue = new PriorityQueue<>();
+
+        // start timer for linkedList
+        Instant start = Instant.now();
 
         // add 100 elements
         for (int i = 0; i < 101; i++) {
@@ -106,13 +136,18 @@ class HashmapsExercise02 {
         // search each element
         for (int j = 100; j > 0; j--) {
             boolean searchQueue = intQueue.contains(j);
-            System.out.println("Queue contains " + j + " " + searchQueue);
+           // System.out.println("Queue contains " + j + " " + searchQueue);
         }
 
         // delete each element
         for (int k = 100; k > 0; k--) {
             intQueue.remove(k);
         }
+
+        // stop timer
+        Instant finish = Instant.now();
+        long queueTimer = Duration.between(start,finish).toNanos();
+        System.out.println("Time to execute Queue tasks " + queueTimer + " nano seconds");
 
         return intQueue;
     }
@@ -121,6 +156,9 @@ class HashmapsExercise02 {
     public static HashMap<Integer, Integer> intHashmap() {
 
         HashMap<Integer, Integer> intHashmap = new HashMap<>();
+
+        // start timer for linkedList
+        Instant start = Instant.now();
 
         // add 100 elements
         for (int i = 0; i < 101; i++) {
@@ -135,13 +173,18 @@ class HashmapsExercise02 {
         // search each element
         for (int k = 0; k < 101; k++) {
             int searchMap = intHashmap.get(k);
-            System.out.println("Hashmap contains " + k + " " + searchMap);
+           // System.out.println("Hashmap contains " + k + " " + searchMap);
         }
 
         // delete each element
         for (int l = 0; l < 101; l++) {
             intHashmap.remove(l);
         }
+
+        // stop timer
+        Instant finish = Instant.now();
+        long mapTimer = Duration.between(start,finish).toNanos();
+        System.out.println("Time to execute Hashmap tasks " + mapTimer + " nano seconds");
 
         return intHashmap;
     }
