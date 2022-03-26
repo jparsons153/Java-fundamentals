5b2c780ed1e6c389ee3ebbd9ac0b674d028d90a5
jparsons153 <jparsons153@hotmail.com>
2022-03-26 15:34:20 +0000

    feat -- added methods for passengers queries, fixed dateTime issue

--- src/mysql/labs/Exercise_04.java
+++ src/mysql/labs/Exercise_04.java
@@ -22,6 +22,7 @@ package mysql.labs;
  */
 
 import java.sql.*;
+import java.time.*;
 
 public class Exercise_04 {
 
@@ -32,13 +33,13 @@ public class Exercise_04 {
 
         createFlight(...); - done
         queryFlight(...); - done
-        updateFlight(...); 
-        deleteFlight(...);
+        updateFlight(...); - done
+        deleteFlight(...); - done
 
-        createPassenger(...);
-        queryPassenger(...);
-        updatePassenger(...);
-        deletePassenger(...);
+        createPassenger(...); - done
+        queryPassenger(...); - done
+        updatePassenger(...); - done
+        deletePassenger(...); - done
         ...
 
          */
@@ -52,7 +53,7 @@ public class Exercise_04 {
             // This will load the MySQL driver, each DB has its own driver
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            String connectionString = "jdbc:mysql://localhost/university?"
+            String connectionString = "jdbc:mysql://localhost/airtravel?"
                     + "user=root&password= ******"  // insert password
                     + "&allowPublicKeyRetrieval=true";
 
@@ -61,7 +62,19 @@ public class Exercise_04 {
             // call methods here within try / catch block
             createFlight(connection, preparedStatement);
 
-            queryFlights(connection,statement, resultSet);
+            queryFlights(connection, statement, resultSet);
+
+            updateFlight(connection, preparedStatement);
+
+            deleteFlight(connection, preparedStatement);
+
+            createPassenger(connection, preparedStatement);
+
+            queryPassenger(connection, statement, resultSet);
+
+            updatePassenger(connection, preparedStatement);
+
+            deletePassenger(connection, preparedStatement);
 
         } catch (SQLException exc) {
             System.out.println("Exception occurred");
@@ -81,28 +94,28 @@ public class Exercise_04 {
         }
     }
 
-    private static void createFlight(Connection connection, PreparedStatement preparedStatement) throws SQLException{
+    private static void createFlight(Connection connection, PreparedStatement preparedStatement) throws SQLException {
 
-            preparedStatement = connection.prepareStatement
-                    ("Insert into airtravel database(flightindex,Flight_no,departure_time,arrival_time,Airport_depart, Airport_arrive,NumPassengers, PlaneID))" + "values (?,?,?,?,?,?,?,?)");
-            preparedStatement.setInt(1, 18);
-            preparedStatement.setString(2, "BA314");
-            preparedStatement.setTime(3, 10:15:00, 2022 - 03 - 21); // date Time method?
-            preparedStatement.setTime(4, 12:15:00, 2022 - 03 - 21); // date Time method?
-            preparedStatement.setString(5, "LHR");
-            preparedStatement.setString(6, "MUN");
-            preparedStatement.setInt(7, 399);
-            preparedStatement.setInt(8, 101);
-            preparedStatement.executeUpdate();
+        preparedStatement = connection.prepareStatement
+                ("Insert into airtravel.flights database(flightindex,Flight_no,departure_time,arrival_time,Airport_depart, Airport_arrive,NumPassengers, PlaneID))" + "values (?,?,?,?,?,?,?,?)");
+        preparedStatement.setInt(1, 18);
+        preparedStatement.setString(2, "BA314");
+        preparedStatement.setTimestamp(3, java.sql.Timestamp.valueOf("2022-03-21 10:15:00"));
+        preparedStatement.setTimestamp(4, java.sql.Timestamp.valueOf("2022-03-21 12:15:00"));
+        preparedStatement.setString(5, "LHR");
+        preparedStatement.setString(6, "MUN");
+        preparedStatement.setInt(7, 399);
+        preparedStatement.setInt(8, 101);
+        preparedStatement.executeUpdate();
     }
 
-    private static void queryFlights(Connection connection, Statement statement, ResultSet resultSet) throws SQLException{
+    private static void queryFlights(Connection connection, Statement statement, ResultSet resultSet) throws SQLException {
 
         statement = connection.createStatement();
 
         resultSet = statement.executeQuery("select * from airtravel.flights;");
 
-        while(resultSet.next()){
+        while (resultSet.next()) {
             System.out.println(resultSet.getInt("flightindex"));
             System.out.println(resultSet.getString("Flight_no"));
             System.out.println(resultSet.getTime("departure_time"));
@@ -113,4 +126,66 @@ public class Exercise_04 {
             System.out.println(resultSet.getInt("PlaneID"));
         }
     }
+
+    private static void updateFlight(Connection connection, PreparedStatement preparedStatement) throws SQLException {
+        preparedStatement = connection.
+                prepareStatement("update airtravel.flights set airport = ? where airport depart = ?");
+        preparedStatement.setString(5, "LGW");
+        int x = preparedStatement.executeUpdate();
+        System.out.println(x);
+    }
+
+    private static void deleteFlight(Connection connection, PreparedStatement preparedStatement) throws SQLException {
+        preparedStatement = connection.
+                prepareStatement("delete from airtravel.flights where flight Index = ?");
+        preparedStatement.setInt(1, 15);
+        System.out.println(preparedStatement.executeUpdate());
+    }
+
+    private static void createPassenger(Connection connection, PreparedStatement preparedStatement) throws SQLException {
+
+        preparedStatement = connection.prepareStatement
+                ("Insert into airtravel.passengers database(Booking_ref, First_name, Last_name, Date_of_birth, Address, Passengers, Flight_details, Meal_preference))" + "values (?,?,?,?,?,?,?,?)");
+        preparedStatement.setInt(1, 110110);
+        preparedStatement.setString(2, "Jill");
+        preparedStatement.setString(3, "Bloggs");
+        preparedStatement.setDate(4, java.sql.Date.valueOf("1953-09-01"));
+        preparedStatement.setString(5, "10 Northumberland Street");
+        preparedStatement.setInt(6, 1);
+        preparedStatement.setString(8, "N/A");
+        preparedStatement.executeUpdate();
+    }
+
+    private static void queryPassenger(Connection connection, Statement statement, ResultSet resultSet) throws SQLException {
+
+        statement = connection.createStatement();
+
+        resultSet = statement.executeQuery("select * from airtravel.passengers;");
+
+        while (resultSet.next()) {
+            System.out.println(resultSet.getInt("Booking_ref"));
+            System.out.println(resultSet.getString("First_name"));
+            System.out.println(resultSet.getString("Last_name"));
+            System.out.println(resultSet.getDate("Date_of_birth"));
+            System.out.println(resultSet.getString("Address"));
+            System.out.println(resultSet.getInt("Passengers"));
+            System.out.println(resultSet.getString("Flight_details"));
+            System.out.println(resultSet.getString("Meal_preference"));
+        }
+    }
+
+    private static void updatePassenger(Connection connection, PreparedStatement preparedStatement) throws SQLException {
+        preparedStatement = connection.
+                prepareStatement("update airtravel.passenger set First Name = ? where Last Name = ?");
+        preparedStatement.setString(2, "Kate");
+        System.out.println(preparedStatement.executeUpdate());
+    }
+
+    private static void deletePassenger(Connection connection, PreparedStatement preparedStatement) throws SQLException {
+        preparedStatement = connection.
+                prepareStatement("delete from airtravel.passenger where Last Name = ?");
+        preparedStatement.setString(3, "Norris");
+        System.out.println(preparedStatement.executeUpdate());
+    }
+
 }
