35d02c18af11743210a0ba5a6e54169eec83959d
jparsons153 <jparsons153@hotmail.com>
2022-03-31 20:02:36 +0100

    Exercise 02 - functional interfaces

--- src/labs_examples/lambdas/labs/Exercise_02.java
+++ src/labs_examples/lambdas/labs/Exercise_02.java
@@ -3,7 +3,6 @@ package labs_examples.lambdas.labs;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.*;
-import java.util.stream.IntStream;
 
 /**
  * Lambdas Exercise 2:
@@ -19,16 +18,16 @@ class Photo {
         int width = 4700;
         int height = 3133;
 
-        //#1
+        //#1 BiFunction
         BiFunction<Integer, Integer, Integer> pixelCalculate = ((w, h) -> w.intValue() * h.intValue());
         int totalPixel = pixelCalculate.apply(width, height);
         System.out.println(totalPixel + " pixels");
 
-        //#2
+        //#2 IntToDouble Function
         IntToDoubleFunction pixelToMB = (p) -> (p*3)/(1024*1024);
         System.out.println(pixelToMB.applyAsDouble(totalPixel) + " MB");
 
-        //#3
+        //#3 BiPredicate
         BiPredicate<Integer, Integer> lessThanMaxInstaSize = (w, h) -> (w.intValue() | h.intValue()) <= 1080;
         System.out.println("Is image less than max 1080 pixels in length or width: " +lessThanMaxInstaSize.test(width, height));
 
@@ -37,19 +36,29 @@ class Photo {
         List<String> aspectRatios = Arrays.asList("16:9 Landscape", "1:1 Square", "4:5 Vertical", "2:3 Vertical", "9:16 Full Portrait");
         aspectRatios.forEach(ratio -> System.out.println(ratio));
 
-        // print colours of each pixel by RGB (BIConsumer)
-        // RGB to BinaryString (Supplier)
+        // #5 Supplier
+        Supplier<Double> PI = () -> Math.PI;
+        System.out.println(PI.get());
 
-        //#5
-        // change RGB to greyScale (Grayscale = 0.299R + 0.587G + 0.114B)
-        int[] teal = {0,128,128};
-        int[] mango = {255, 205, 72};
-        int[] lavender = {181, 126, 220};
+        // #6 Predicate
+        Predicate<Integer> lessThanMax = a -> (a < width);
+        System.out.println(lessThanMax.test(4200));
 
-        //Function<Integer[],Integer> toGreyScale = (r,g,b) -> (r.)
+        // #7 Function
+        Function<Integer, Integer> doubleWidth = w -> w*2;
+        System.out.println(doubleWidth.apply(width));
 
+        // #8 Double to Int
+        DoubleToIntFunction pixelToWidth = p -> (int)(p/ height);
+        System.out.println(pixelToWidth.applyAsInt(width));
 
+        // #9 Unary operator
+        UnaryOperator<Integer> multiplyTen = p -> p*10;
+        System.out.println(multiplyTen.apply(totalPixel));
 
+        // #10 Double Consumer
+        DoubleConsumer pixel = (p) -> System.out.println((p*3)/(1024*1024));
+        pixel.accept(width);
 
     }
 }
